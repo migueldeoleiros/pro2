13b58e80bb724c7d2a5a397902e4050000ebe5bf
DanielFernandezFeas <98942162+DanielFernandezFeas@users.noreply.github.com>
2022-02-07 10:41:30 +0100

    Update rational_pointer.c

--- p0/rational_pointer.c
+++ p0/rational_pointer.c
@@ -2,7 +2,7 @@
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 0
  * AUTHOR 1: MIGUEL LOPEZ LOPEZ            LOGIN 1: m.llopez
- * AUTHOR 2:                               LOGIN 2:
+ * AUTHOR 2: DANIEL FERNÁNDEZ FEÁS         LOGIN 2: daniel.fernandezf
  * GROUP: 4.4
  * DATE: 11 / 2 / 2022
  */
