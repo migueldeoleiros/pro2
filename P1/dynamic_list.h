8714c2e82b3b9a49d03a2af29cee6e75ef540d02
migueldeoleiros <migueldeoleiros@gmail.com>
2022-02-20 14:51:13 +0100

    archivos para empezar la p1

---
+++ P1/dynamic_list.h
@@ -0,0 +1,17 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 1
+ * AUTHOR 1: ***************************** LOGIN 1: **********
+ * AUTHOR 2: ***************************** LOGIN 2: **********
+ * GROUP: *.*
+ * DATE: ** / ** / **
+ */
+
+#ifndef DYNAMIC_LIST_H
+#define DYNAMIC_LIST_H
+
+#include "types.h"
+
+/* Write your code here... */
+
+#endif
