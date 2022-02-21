e28700e2461583f07176e5d3c77b39ca99f69c01
migueldeoleiros <migueldeoleiros@gmail.com>
2022-02-21 10:07:01 +0100

    añadido el nombre a fichero main.c

--- p1/main.c
+++ p1/main.c
@@ -1,10 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: MIGUEL LOPEZ LOPEZ            LOGIN 1: m.llopez
+ * AUTHOR 2: DANIEL FERNÁNDEZ FEÁS         LOGIN 2: daniel.fernandezf
+ * GROUP: 4.4
+ * DATE: ** / ** / 2022
  */
 
 #include <stdio.h>
@@ -86,9 +86,3 @@ int main(int nargs, char **args) {
 
     return 0;
 }
-
-
-
-
-
-
