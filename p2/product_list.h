dbd86cccd8f22825920033078bcd9a08a517971d
migueldeoleiros <migueldeoleiros@gmail.com>
2022-03-30 11:21:21 +0200

    nombres en cada archivo

--- p2/product_list.h
+++ p2/product_list.h
@@ -1,9 +1,9 @@
 /*
  * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
+ * SUBTITLE: Practical 1
+ * AUTHOR 1: MIGUEL LOPEZ LOPEZ            LOGIN 1: m.llopez
+ * AUTHOR 2: DANIEL FERNÁNDEZ FEÁS         LOGIN 2: daniel.fernandezf
+ * GROUP: 4.4
  * DATE: ** / ** / **
  */
 
