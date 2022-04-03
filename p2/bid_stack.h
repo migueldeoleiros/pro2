b8698ce30e88dec7372f8ebf443ff159cb75c7e7
DanielFernandezFeas <98942162+DanielFernandezFeas@users.noreply.github.com>
2022-04-03 18:21:36 +0200

    Update bid_stack.h

--- p2/bid_stack.h
+++ p2/bid_stack.h
@@ -29,13 +29,37 @@ typedef struct Stack {
 } tStack;
 
 void createEmptyStack(tStack *stack);
+/* Crea una pila vacia
+* Entrada: Pila a inicializar
+* Salida: Pila inicializada
+* Postcondicion: La pila no tienen elementos
+*/
 
 bool push(tItemS item, tStack *stack);
+/* Inserta un elemento en la cima de la pila. Devuelve un valor true si el elemento fue apilado; false en caso contrario.
+* Entrada: El elemento a insertar y la pila 
+* Salida: True en caso de que se pudiera insertar el elemento 
+*         Falso en caso de que no se pudiera insertar el elemento
+*/
 
 void pop(tStack *stack);
+/* Elimina de la pila el elemento situado en la cima.
+* Entrada: Pila a procesar
+* Salida: Pila con un elemento menos
+* Precondicion: La pila no esta vacia
+*/
 
 tItemS peek(tStack stack);
+/*Recupera el elemento de la cima de la pila sin eliminarlo.
+* Entrada: Pila a procesar
+* Salida: Elemento situado en la cima de la pila
+* Precondicion: La pila no esta vacia
+*/
 
 bool isEmptyStack(tStack stack);
-
+/* Determina si una pila está vacía o no.
+* Entrada: Pila a procesar
+* Salida: True si esta vacia 
+*         False si contiene algun elemento
+*/
 #endif
